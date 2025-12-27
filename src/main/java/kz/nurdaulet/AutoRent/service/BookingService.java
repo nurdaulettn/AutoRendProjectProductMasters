c95523d9664ee9f36f76ccc40b0dc3a38a9790aa
nurdaulettn <nurdaulettolegen26@gmail.com>
2025-12-27 15:53:52 +0500

    fix some bugs

--- src/main/java/kz/nurdaulet/AutoRent/service/BookingService.java
+++ src/main/java/kz/nurdaulet/AutoRent/service/BookingService.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
